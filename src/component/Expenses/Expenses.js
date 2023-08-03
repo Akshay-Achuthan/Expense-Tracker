85e0c925057d9aee26292e59fdd51684e663c779
akshayachuthan <akshayacuthan@credenceanalytics.com>
2023-08-03 23:24:54 +0530

    list rendering dynamic data

--- src/component/Expenses/Expenses.js
+++ src/component/Expenses/Expenses.js
@@ -20,26 +20,13 @@ const Expenses = (props) => {
       selected={filteredYear}
       onChangeFilter={filterChangeHandler}
       />
+      {props.items.map((expense) => (
         <ExpenseItem 
-        title={props.items[0].title}
-        date={props.items[0].date}
-        amount={props.items[0].amount}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
         />
-        <ExpenseItem 
-        title={props.items[1].title}
-        date={props.items[1].date}
-        amount={props.items[1].amount}
-        />
-        <ExpenseItem 
-        title={props.items[2].title}
-        date={props.items[2].date}
-        amount={props.items[2].amount}
-        />
-        <ExpenseItem 
-        title={props.items[3].title}
-        date={props.items[3].date}
-        amount={props.items[3].amount}
-        />
+      ))}
       </Card>
   );
 }
--- src/component/Expenses/ExpensesItem.js
+++ src/component/Expenses/ExpensesItem.js
@@ -16,7 +16,7 @@ const ExpenseItem = (props) => {
       <ExpensesDate date={props.date}/>
       <div className="expense-item__description">
         <h2>{title}</h2>
-        <div className="expense-item__price">{props.amount}</div>
+        <div className="expense-item__price">${props.amount}</div>
         <button onClick={clickHandler}>Change title</button>
       </div>
     </Card>
