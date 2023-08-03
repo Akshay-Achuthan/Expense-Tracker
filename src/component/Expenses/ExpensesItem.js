a61be959d9200246205ca770dcb1148bbff7bfa2
akshayachuthan <akshayacuthan@credenceanalytics.com>
2023-08-04 02:03:32 +0530

    removed unwanted codes

--- src/component/Expenses/ExpensesItem.js
+++ src/component/Expenses/ExpensesItem.js
@@ -1,24 +1,18 @@
-import React, {useState} from "react";
+import React from "react";
 import Card from "../UI/Card"
 import ExpensesDate from "./ExpensesDate";
 
 import "./ExpensesItem.css"
 
 const ExpenseItem = (props) => {
-  const [title,setTitle] = useState(props.title);
-
-  const clickHandler = () => {
-    setTitle('updated!');
-  }
 
   return (
     <li>
     <Card className="expense-item">
       <ExpensesDate date={props.date}/>
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
-        <button onClick={clickHandler}>Change title</button>
       </div>
     </Card>
     </li>
