09e145ef44f1799e28c429d8cad90b7cef4b9e2d
akshayachuthan <akshayacuthan@credenceanalytics.com>
2023-08-03 11:44:58 +0530

    wrapping components with newly created wrapper component

--- src/component/ExpensesItem.js
+++ src/component/ExpensesItem.js
@@ -1,16 +1,17 @@
 import ExpensesDate from "./ExpensesDate";
+import Card from "./Card"
 
 import "./ExpensesItem.css"
 
 const ExpenseItem = (props) => {
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpensesDate date={props.date}/>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
